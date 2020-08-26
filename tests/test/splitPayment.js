09c61ea2676e3eaeeab8730d332afe383e723155
thelonehegelian <thelonehegelian@gmail.com>
2020-08-26 13:13:58 +0100

    Write and Pass should split payment test

--- tests/test/splitPayment.js
+++ tests/test/splitPayment.js
@@ -5,4 +5,32 @@ contract("SplitPayment", (accounts) => {
 	before(async () => {
 		splitPayment = await SplitPayment.deployed();
 	});
+	it("Should split the payments", async () => {
+		const recipients = [accounts[1], accounts[2], accounts[3]];
+		const amounts = [40, 20, 30];
+
+		const initialBalances = await Promise.all(
+			recipients.map((recipient) => {
+				return web3.eth.getBalance(recipient);
+			})
+		);
+		await splitPayment.send(recipients, amounts, {
+			from: accounts[0],
+			value: 90,
+		});
+
+		const finalBalances = await Promise.all(
+			recipients.map((recipient) => {
+				return web3.eth.getBalance(recipient);
+			})
+		);
+		recipients.forEach((_item, i) => {
+			const finalBalance = web3.utils.toBN(finalBalances[i]);
+			const initialBalance = web3.utils.toBN(initialBalances[i]);
+			assert.equal(
+				finalBalance.sub(initialBalance).toNumber(),
+				amounts[i]
+			);
+		});
+	});
 });
